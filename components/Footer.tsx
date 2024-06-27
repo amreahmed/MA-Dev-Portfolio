cad332124e4f2c2faa74e1fb9ddd338e242d3b93
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-06-27 06:24:43 +0300

    commit 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import MagicButton from './ui/MagicButton';
-import { FaLocationArrow } from 'react-icons/fa';
-import { socialMedia } from '@/data';
-import Image from 'next/image';
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import Image from "next/image";
 
 export const Footer = () => {
   return (
@@ -25,13 +25,14 @@ export const Footer = () => {
         <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 MA Dev</p>
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
-            <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-              
-              <Image src={profile.img} alt='1' width={20} height={20} />
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+              <Image src={profile.img} alt="1" width={20} height={20} />
             </div>
           ))}
         </div>
       </div>
     </footer>
   );
-}
+};
